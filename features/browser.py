72f195a13064304823c1b67bd8bf2921238a8f20
jack <jblacketter@gmail.com>
2019-05-14 12:49:01 -0700

    updating webdriver to use selenium grid

--- features/browser.py
+++ features/browser.py
@@ -2,12 +2,11 @@ from selenium import webdriver
 
 class Browser(object):
 
-    #driver = webdriver.Firefox()
-    driver = webdriver.Remote(
-        command_executor='http://127.0.0.1:4444/wd/hub',
-        desired_capabilities={'browserName': 'firefox', 'javascriptEnabled': True})
-
+    # driver = webdriver.Firefox()
     # driver = webdriver.Chrome()
+    driver = webdriver.Remote(
+    command_executor='http://127.0.0.1:4444/wd/hub',
+    desired_capabilities={'browserName': 'firefox', 'javascriptEnabled': True})
     driver.implicitly_wait(40)
     driver.set_page_load_timeout(40)
     driver.maximize_window()
